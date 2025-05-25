225c89b0c9deb76ad1121b13af413a498736abda
Dafnye <dafny.lira@aluno.faculdadeimpacta.com.br>
2025-05-24 22:16:38 -0300

    feat: cria componente Home com CSS

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,25 @@
+import { Link, useNavigate } from 'react-router-dom';
+import './home.css';
+import { useRef, useState } from 'react';
+import api from '../../services/api';
+
+function Home() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="home-container">
+            <h1>Bem-vindo à plataforma do <span className="highlight">Mercado X</span>!</h1>
+            <p>Escolha uma das opções abaixo para continuar:</p>
+            <div className="button-group">
+                <button className="button" onClick={() => navigate('/cadastro-seller')}>
+                    Cadastrar Seller
+                </button>
+                <button className="button" onClick={() => navigate('/login')}>
+                    Login
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Home
